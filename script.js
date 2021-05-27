ec0a858212dd53850233f9ca944f2bf1b30386d1
itsdeebarnett <jdeebarnett@gmail.com>
2021-05-27 08:56:12 -0400

    startingover

--- script.js
+++ script.js
@@ -34,3 +34,37 @@ const currencies = [
   'MYR'
 ]
 
+const URL = 'http://api.exchangeratesapi.io/v1/latest?access_key=757feee5e2d8f3d429f3ebfbbdd9c251'
+
+const root = document.querySelector('.container')
+
+
+let header = document.createElement('div')
+
+// header.innerHTML = <h1>${'Currency Convertor Plus'}</h1>
+//debug line 44 still an error 
+
+
+
+//wow--I suck 
+
+let currencyFrom = document.getElementById('currency-name')
+
+
+
+fetch(URL)
+  .then(res => res.json())
+  .then(data => { 
+    console.log(data)
+  let h2 = document.createElement('h2')
+  h2.innerHTML = data.json
+  
+  return (data)
+  
+
+  for (let currency of currencies) {
+    let currency_select= document.createElement('Selection')
+    
+
+  }
+  })
